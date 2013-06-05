bf8e280255bd6d4f0e2343ba592ee90171163daa
Jayce Dowell <jayce.dowell@gmail.com>
2013-06-05 04:26:42 +0000

    Added a new function to the _psr extension, SumPolWithOverflow(), to help deal with polarizaton summing.  Fixed the documentation for the PulsarEngine() function.

--- psr.c
+++ psr.c
@@ -24,7 +24,7 @@
 #define imaginary _Complex_I
 
 
-static PyObject *FPSDC2(PyObject *self, PyObject *args, PyObject *kwds) {
+static PyObject *PulsarEngine(PyObject *self, PyObject *args, PyObject *kwds) {
 	PyObject *signals, *signalsF;
 	PyArrayObject *data, *dataF;
 	int nChan = 64;
@@ -122,7 +122,7 @@ static PyObject *FPSDC2(PyObject *self, PyObject *args, PyObject *kwds) {
 	return signalsF;
 }
 
-PyDoc_STRVAR(FPSDC2_doc, \
+PyDoc_STRVAR(PulsarEngine_doc, \
 "Perform a series of Fourier transforms on complex-valued data to get sub-\n\
 integration data\n\
 \n\
@@ -131,12 +131,107 @@ Input arguments are:\n\
 \n\
 Input keywords are:\n\
  * LFFT: number of FFT channels to make (default=64)\n\
- * ClipLevel: count value of 'bad' data.  FFT windows with instantaneous powers\n\
-   greater than or equal to this value greater are zeroed.  Setting the ClipLevel\n\
-   to zero disables time-domain blanking\n\
 \n\
 Outputs:\n\
- * sub-integration: 2-D numpy.double (stands by channels) of FFT'd data\n\
+ * sub-integration: 2-D numpy.uint8 (stands by channels) of FFT'd data\n\
+");
+
+
+static PyObject *SumPolsWithOverflow(PyObject *self, PyObject *args, PyObject *kwds) {
+	PyObject *spectra, *spectraF;
+	PyArrayObject *data, *dataF;
+	long i, index0, index1, nStands, nSamps;
+	
+	static char *kwlist[] = {"spectra", "index0", "index1", NULL};
+	if(!PyArg_ParseTupleAndKeywords(args, kwds, "Oii", kwlist, &spectra, &index0, &index1)) {
+		PyErr_Format(PyExc_RuntimeError, "Invalid parameters");
+		return NULL;
+	}
+	
+	// Bring the data into C and make it usable
+	data = (PyArrayObject *) PyArray_ContiguousFromObject(spectra, NPY_UINT8, 2, 2);
+	
+	// Get the properties of the data
+	nStands = (long) data->dimensions[0];
+	nSamps = (long) data->dimensions[1];
+	
+	// Validation
+	if( index0 < 0 || index0 >= nStands ) {
+		PyErr_Format(PyExc_ValueError, "index0 is out-of-range");
+		Py_XDECREF(data);
+		return NULL;
+	}
+	if( index1 < 0 || index1 >= nStands ) {
+		PyErr_Format(PyExc_ValueError, "index1 is out-of-range");
+		Py_XDECREF(data);
+		return NULL;
+	}
+	if( index0 == index1 ) {
+		PyErr_Foramt(PyExc_ValueError, "index1 cannot equal index0");
+		Py_XDECREF(data);
+		return NULL;
+	}
+	
+	// Find out how large the output array needs to be and initialize it
+	npy_intp dims[1];
+	dims[0] = (npy_intp) nSamps;
+	dataF = (PyArrayObject*) PyArray_SimpleNew(1, dims, NPY_UINT8);
+	if(dataF == NULL) {
+		PyErr_Format(PyExc_MemoryError, "Cannot create output array");
+		Py_XDECREF(data);
+		return NULL;
+	}
+	PyArray_FILLWBYTE(dataF, 0);
+	
+	// Integer delay, FFT, and fractional delay
+	unsigned char *a, *b;
+	unsigned short int tempV;
+	a = (unsigned char *) data->data;
+	b = (unsigned char *) dataF->data;
+	
+	#ifdef _OPENMP
+		#ifdef _MKL
+			fftw3_mkl.number_of_user_threads = omp_get_num_threads();
+		#endif
+		#pragma omp parallel default(shared) private(i, tempV)
+	#endif
+	{
+		#ifdef _OPENMP
+			#pragma omp for schedule(static)
+		#endif
+		for(i=0; i<nSamps; i++) {
+			tempV = (unsigned short) *(a + index0*nSamps + i) + (unsigned short) *(a + index1*nSamps + i);
+			if( tempV > 255 ) {
+				*(b + i) = 255;
+			} else {
+				*(b + i) = (unsigned char) tempV;
+			}
+		}
+	}
+	
+	Py_XDECREF(data);
+	
+	spectraF = Py_BuildValue("O", PyArray_Return(dataF));
+	Py_XDECREF(dataF);
+
+	return spectraF;
+}
+
+PyDoc_STRVAR(SumPolsWithOverflow_doc, \
+"Given a 2-D numpy.unit8 array created by PulsarEngine() and two indicies\n\
+along the zeroth axis that specify the two polarization, sum the\n\
+polarizations.\n\
+\n\
+Input arguments are:\n\
+ * signals: 2-D numpy.uint8 (stands by samples) array of sub-integration data\n\
+ * index0: integer index of the first polarization\n\
+ * index1: integer index of the second polarization\n\
+\n\
+Input keywords are:\n\
+ None\n\
+\n\
+Outputs:\n\
+ * sub-integration: 1-D numpy.uint8 (stands by channels) of the summed data\n\
 ");
 
 
@@ -145,7 +240,8 @@ Outputs:\n\
 */
 
 static PyMethodDef SpecMethods[] = {
-	{"PulsarEngine",  (PyCFunction) FPSDC2,  METH_VARARGS|METH_KEYWORDS, FPSDC2_doc}, 
+	{"PulsarEngine",        (PyCFunction) PulsarEngine,        METH_VARARGS|METH_KEYWORDS, PulsarEngine_doc}, 
+	{"SumPolsWithOverflow", (PyCFunction) SumPolsWithOverflow, METH_VARARGS,               SumPolsWithOverflow_doc},
 	{NULL,      NULL,    0,                          NULL      }
 };
 
@@ -156,6 +252,8 @@ The functions defined in this module are:\n\
   * PulsarEngine - FFT and integrate function for computing a series of\n\
     Fourier transforms for a complex-valued (TBN and DRX) signal from a \n\
     collection of stands/beams all at once.\n\
+  * SumPolsWithOverflow - Sum two polarization stored in an unsigned\n\
+    byte array being mindful of overflows.\n\
 \n\
 See the inidividual functions for more details.");
 
